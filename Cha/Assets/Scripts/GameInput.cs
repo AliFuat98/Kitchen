17f5db7bc795c3fcfde6bec943252d7c7e8f6625
Ali Fuat <87227396+AliFuat98@users.noreply.github.com>
2023-06-20 23:25:20 +0300

    singleton and selected counter visual

--- Cha/Assets/Scripts/GameInput.cs
+++ Cha/Assets/Scripts/GameInput.cs
@@ -1,11 +1,20 @@
+using System;
 using UnityEngine;
 
 public class GameInput : MonoBehaviour {
+
+  public event EventHandler OnInteratAction;
+
   private PlayerInputActions playerInputActions;
 
   private void Awake() {
     playerInputActions = new PlayerInputActions();
     playerInputActions.Player.Enable();
+    playerInputActions.Player.Interact.performed += Interact_performed;
+  }
+
+  private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
+    OnInteratAction?.Invoke(this, EventArgs.Empty);
   }
 
   public Vector2 GetMovementVectorNormalized() {
--- Cha/Assets/Scripts/Player.cs
+++ Cha/Assets/Scripts/Player.cs
@@ -1,16 +1,82 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+  public static Player Instance { get; private set; }
+
+  public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+
+  public class OnSelectedCounterChangedEventArgs : EventArgs {
+    public ClearCounter selectedCounter;
+  }
+
   [SerializeField] private float moveSpeed = 7f;
+
   [SerializeField] private GameInput gameInput;
+  [SerializeField] private LayerMask countersLayerMask;
 
   private bool isWalking;
+  private Vector3 lastInteractDir;
+  private ClearCounter selectedCounter;
 
-  public bool IsWalking() {
-    return isWalking;
+  private void Awake() {
+    if (Instance != null) {
+      Debug.LogError("there is more than one player");
+      return;
+    }
+    Instance = this;
+  }
+
+  private void Start() {
+    gameInput.OnInteratAction += GameInput_OnInteratAction;
+  }
+
+  private void GameInput_OnInteratAction(object sender, System.EventArgs e) {
+    if (selectedCounter != null) {
+      selectedCounter.Interact();
+    }
   }
 
   private void Update() {
+    handleMovement();
+    handleInteractions();
+  }
+
+  private void handleInteractions() {
+    Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+    Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+    // hareketi kestiðimizde interact devam etmesi için sonuncu yönü kaydet.
+    if (moveDir != Vector3.zero) {
+      lastInteractDir = moveDir;
+    }
+
+    float interactDistance = 2f;
+    if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)) {
+      if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
+        // has Clear Counte
+        if (clearCounter != selectedCounter) {
+          SetSelectedCounter(clearCounter);
+        }
+      } else {
+        // clear counter scripti yok ise
+        SetSelectedCounter(null);
+      }
+    } else {
+      // there is nothing in the front
+      SetSelectedCounter(null);
+    }
+  }
+
+  private void SetSelectedCounter(ClearCounter selectedCounter) {
+    this.selectedCounter = selectedCounter;
+    OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs {
+      selectedCounter = selectedCounter
+    });
+  }
+
+  private void handleMovement() {
     Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
     Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
@@ -47,11 +113,15 @@ public class Player : MonoBehaviour {
     // X ve Z eksenlerinde engel yok
     if (canMove) {
       transform.position += moveDir * moveDistance;
-    } 
+    }
 
     isWalking = moveDir != Vector3.zero;
 
     float rotationSpeed = 10f;
     transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
+  }
+
+  public bool IsWalking() {
+    return isWalking;
   }
 }
