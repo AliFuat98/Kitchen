7f3896c59cf306183214864aa0f7d59b418601e3
Ali Fuat <87227396+AliFuat98@users.noreply.github.com>
2023-06-21 13:52:53 +0300

    kitchen object assign a parent

--- Cha/Assets/Scripts/ClearCounter.cs
+++ Cha/Assets/Scripts/ClearCounter.cs
@@ -2,13 +2,71 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour {
 
+  /// bu kutunun üzerinde spawn edeceðimiz scriptable obje (malzeme)
   [SerializeField] private KitchenObjectSO kitchenObjectSO;
-  [SerializeField] private Transform counterTopPoint;
-  public void Interact() {
-    Debug.Log(transform.name);
-    Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-    kitchenObjectTransform.localPosition = Vector3.zero;
 
-    Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+  /// malzemeyi nerde spawn edicez = kutunun tepesi
+  [SerializeField] private Transform counterTopPoint;
+
+  /// kutunun üzerindeki malzeme
+  private KitchenObject kitchenObject;
+
+  /// taþýma yapýcaðýmýz ikinci kutu (TEST)
+  [SerializeField] private ClearCounter secondClearCounter;
+
+  [SerializeField] private bool testing;
+
+  private void Update() {
+    if (testing && Input.GetKeyDown(KeyCode.T)) {
+      if (kitchenObject != null) {
+        // kutu üzeri dolu
+
+        // ikici test kutusuna ýþýnla
+        kitchenObject.SetClearCounter(secondClearCounter);
+      }
+    }
+  }
+
+  public void Interact() {
+    if (kitchenObject == null) {
+      // kutunun üzeri boþ
+
+      // malzemeyi oluþtur
+      Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+      kitchenObjectTransform.localPosition = Vector3.zero;
+
+      // malzemenin hangi kutunun üzerinde olduðunu iþaretle
+      kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+    } else {
+      // kutunun üzeri dolu
+
+      // hangi kutu bastýr
+      Debug.Log(kitchenObject.GetClearCounter());
+    }
+  }
+
+  /// kutunun spawn noktasýný dön
+  public Transform GetKitchenObjectFollowTransform() {
+    return counterTopPoint;
+  }
+
+  /// kutunun üzerindeki malzemeyi deðiþtir
+  public void SetKitchenObject(KitchenObject kitchenObject) {
+    this.kitchenObject = kitchenObject;
+  }
+
+  /// kutunun üzerindeki malzemeyi dön
+  public KitchenObject GetKitchenObject() {
+    return kitchenObject;
+  }
+
+  /// kutunun üzerini temizle
+  public void ClearKitchenObject() {
+    kitchenObject = null;
+  }
+
+  /// kutunun üzerinde bir malzeme var mý?
+  public bool HasKitchenObject() {
+    return kitchenObject != null;
   }
 }
--- Cha/Assets/Scripts/KitchenObject.cs
+++ Cha/Assets/Scripts/KitchenObject.cs
@@ -3,7 +3,39 @@ using UnityEngine;
 public class KitchenObject : MonoBehaviour {
   [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
+  /// üzerinde bulunduðumuz kutu
+  private ClearCounter clearCounter;
+
   public KitchenObjectSO GetKitchenObjectSO() {
     return kitchenObjectSO;
+  }
+
+  /// baþka kutuya ýþýnlanma
+  public void SetClearCounter(ClearCounter clearCounter) {
+    if (this.clearCounter != null) {
+      // bir kutunun üzerindeyiz
+
+      // üzerinde bulunduðumuz eski kutuyu temizle
+      this.clearCounter.ClearKitchenObject();
+    }
+
+    // yeni kutumuzu iþaretle
+    this.clearCounter = clearCounter;
+
+    // yeni kutuya kendimizi iþaretle
+    if (clearCounter.HasKitchenObject()) {
+      Debug.LogError("clear counter has already an object");
+      return;
+    }
+    clearCounter.SetKitchenObject(this);
+
+    // kendini yeni gelen kutuya ýþýnla
+    transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+    transform.localPosition = Vector3.zero;
+  }
+
+  /// üzerinde olduðumuz kutuyu dön
+  public ClearCounter GetClearCounter() {
+    return clearCounter;
   }
 }
